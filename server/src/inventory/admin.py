b830a94a3aab7ff78a87ca36fe199797ffd1d596
William Pierce <wpierce118@gmail.com>
2023-07-15 22:45:16 -0400

    Hierarchical locations

--- server/src/inventory/admin.py
+++ server/src/inventory/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Location
 
-# Register your models here.
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ["name", "hierarchy"]
+
+
+admin.site.register(Location, LocationAdmin)
--- server/src/inventory/models.py
+++ server/src/inventory/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from mptt.models import MPTTModel, TreeForeignKey
 
-# Create your models here.
+
+class Location(MPTTModel):
+    name = models.CharField(max_length=50, unique=True)
+    parent = TreeForeignKey(
+        "self", on_delete=models.CASCADE, null=True, blank=True, related_name="children"
+    )
+
+    def hierarchy(self):
+        ancestors = [x.name for x in self.get_ancestors(include_self=True)]
+        return ":".join(ancestors)
+
+    def __str__(self):
+        return self.name
+
+    class MPTTMeta:
+        order_insertion_by = ["name"]
