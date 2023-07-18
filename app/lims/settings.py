fbfe78404f21b1d1373424631aca55fef086d34b
William Pierce <wpierce118@gmail.com>
2023-07-18 15:51:17 -0400

    Default secret key for building container

--- app/lims/settings.py
+++ app/lims/settings.py
@@ -27,7 +27,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["SECRET_KEY"]
+if "SECRET_KEY" in os.environ:
+    SECRET_KEY = os.environ["SECRET_KEY"]
+else:
+    SECRET_KEY = "django-insecure-^z16o6g&-*kv%q8_tkt5@&iiketx&k@zzp^*89byj_!=s+h=a!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
