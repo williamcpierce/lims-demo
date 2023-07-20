1326144fd23a4267c264e037056758e0ead599bb
William Pierce <wpierce118@gmail.com>
2023-07-19 21:00:55 -0400

    Translation import

--- app/override/admin.py
+++ app/override/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.models import User, Group
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+from django.utils.translation import gettext_lazy as _
 
 from auditlog.registry import auditlog
 from auditlog.models import LogEntry
