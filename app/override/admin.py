ed7b7ff7f4d15caf66e518ec913374486098f8db
William Pierce <wpierce118@gmail.com>
2023-07-19 21:00:19 -0400

    Hide password field in user admin

--- app/override/admin.py
+++ app/override/admin.py
@@ -12,8 +12,26 @@ from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationFo
 
 class UserAdmin(BaseUserAdmin, ModelAdmin):
     """
-    Override default forms with forms provided by Unfold admin.
+    Override default forms with forms provided by Unfold admin, and hide password hash field.
     """
+
+    fieldsets = (
+        (None, {"fields": ("username")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
 
     form = UserChangeForm
     add_form = UserCreationForm
